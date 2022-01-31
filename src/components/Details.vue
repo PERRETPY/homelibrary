fc970ee3f2aca7543a235bd1a1defe20530a7172
perretpy <pierreyves.perret@gmail.com>
2022-01-31 14:37:12 +0100

    Create fake data for details template

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,10 +1,31 @@
 <template>
-  <p>Details component</p>
+  <p>Details component : {{ id }} !</p>
 </template>
 
 <script>
 export default {
-  name: "Details.vue"
+  name: "Details.vue",
+  props: {
+    id: String
+  },
+  data() {
+    return {
+      title: "Mes 10 commandements",
+      authors: [
+        "Kevin Mayer",
+        "Nicolas Herbelot"
+      ],
+      publisher: "Solar",
+      publishedDate: "2020",
+      tags: [],
+      status: "over",
+      description: "Je n'ai pas grandi dans l'idée de vivre un jour de mon sport, ni fait du décathlon pour en détenir le record du monde. Il y a des enfants champions que des parents destinent à ça. Ce n'est pas mon cas.",
+      rate: 4,
+      review: "Super, très intéressant !",
+      available: false,
+      note: "prêté au voisin"
+    }
+  }
 }
 </script>
 
