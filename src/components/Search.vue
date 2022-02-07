1d524a1d616f98a5c300d31c993d5e6b400a5e52
perretpy <pierreyves.perret@gmail.com>
2022-02-07 15:55:29 +0100

    Add search on noNative plateform

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,9 +4,21 @@
       <Scan></Scan>
     </div>
     <div v-else>
-      <h3>Search</h3>
-      <input type="text">
-      <button>Go</button>
+
+      <div class="field">
+        <label class="label">Rechercher un livre</label>
+        <div class="control">
+          <input
+              class="input"
+              v-bind:class="{ 'is-danger': !validForm }"
+              v-model="isbn"
+              type="text"
+              placeholder="ISBN">
+        </div>
+        <p v-if="!validForm" class="help is-danger">This ISBN is not correct</p>
+      </div>
+
+      <button class="button is-primary" @click="onGoClick">Submit</button>
     </div>
   </div>
 </template>
@@ -21,13 +33,30 @@ export default {
   },
   data() {
     return {
+      isbn: null,
+      validForm: true,
       isNativePlatform: Capacitor.isNativePlatform()
+    }
+  },
+  methods: {
+    onGoClick: function () {
+      console.log(this.isbn)
+      const regex = /97[89][0-9]{10}/
+      if ( regex.test(this.isbn) ) {
+        this.validForm = true;
+        this.$router.replace({ path: '/details/'+this.isbn })
+      } else {
+        this.validForm = false;
+        console.log(this.validForm)
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .test {
+    background-color: red;
+  }
 
 </style>
