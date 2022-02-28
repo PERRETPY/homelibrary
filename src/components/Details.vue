60b2928d723899aa30a05c59406eefdc392afea0
perretpy <pierreyves.perret@gmail.com>
2022-02-28 15:46:30 +0100

    wip style detail

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -5,16 +5,19 @@
         <img v-if="book.imageLinks" class="book-cover" v-bind:src="book.imageLinks.thumbnail" alt="">
         <img v-else class="book-cover" src="../assets/book_cover_placeholder.png" alt="">
       </div>
-      <div class="column is-half-tablet">
-        <h2>Titre : <span v-if="book">{{ book.title }}</span></h2>
-        <h3>Auteur : <span v-if="book">{{ book.authors.join(', ') }}</span></h3>
-        <h3>Date de parution : <span v-if="book">{{ book.publishedDate }}</span> </h3>
+      <div class="column is-half-tablet details" v-if="book">
+        <h2>Titre : {{ book.title }}</h2>
+        <h3>Auteur : {{ book.authors.join(', ') }}</h3>
+        <h3>Date de parution : {{ book.publishedDate }}</h3>
         <span class="tag is-info" v-for="tag in tags" :key="tags.indexOf(tag)">
           {{ tag }}
           <button class="delete is-small"></button>
         </span>
+
         <h3>Description : </h3>
-        <p v-if="book">{{ book.description }}</p>
+        <p class="description" v-if="book.description" v-html="book.description"/>
+        <p v-else> pas de description pour le moment</p>
+
         
         <star-rating v-model:rating="selfRate" :increment="0.5"/>
         <div v-if="book">
@@ -112,14 +115,27 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  margin-top: 3em;
-}
+  .container {
+    margin-top: 3em;
+  }
   .book-cover {
     height: 100%;
     max-height: 30em;
   }
   .tag {
     margin-right: 1em;
+  }
+  .description {
+    max-height: 20em;
+    overflow: scroll;
+    box-shadow: 2px 2px 3px 1px rgba(0, 0, 0, 0.2);
+    border-radius: .3em;
+    padding: .3em;
+  }
+  .details {
+    max-height: 100%;
+  }
+  textarea {
+    resize: none;
   }
 </style>
