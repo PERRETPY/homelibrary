edb988dde087277f20869a7e1f2d7fa38773ec96
Pierre-Yves Perret <pierreyves.perret@gmail.com>
2022-03-31 21:45:47 +0200

    Create database

---
+++ src/db.js
@@ -0,0 +1,6 @@
+import Dexie from 'dexie';
+
+export const db = new Dexie('myDatabase');
+db.version(2).stores({
+    books: '+isbn, title, authors, publishedDate, description, tags, ratingsCount, averageRating, selfRate, review, available',
+});
