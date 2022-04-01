3a32414f30f5dfd1b0a18a691376bdff47d49b43
perretpy <pierreyves.perret@gmail.com>
2022-04-01 20:34:36 +0200

    Add view for all book in database

--- src/components/BookPreview.vue
+++ src/components/BookPreview.vue
@@ -1,13 +1,35 @@
 <template>
-$END$
+  <div class="card">
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4">{{ book.title }}</p>
+          <p class="subtitle is-6">{{ book.authors.join(', ') }}</p>
+        </div>
+      </div>
+      <div class="content">
+            <span class="tag is-info" v-for="tag in book.tags" :key="book.tags.indexOf(tag)">
+              {{ tag }}
+            </span>
+        <br>
+        <br>
+        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "BookPreview"
+  name: "BookPreview",
+  props: {
+    book: {}
+  }
 }
 </script>
 
 <style scoped>
-
+.tag {
+  margin-right: .8em;
+}
 </style>
--- src/components/Library.vue
+++ src/components/Library.vue
@@ -1,10 +1,55 @@
 <template>
-  <p>Library component</p>
+  <button @click="getAllTagsFromDatabase()">getAllTagsFromDatabase</button>
+  <div class="columns is-multiline">
+    <div class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
+         v-for="book in booksList"
+         :key="book.isbn">
+      <BookPreview @click="showBooksDetails(book.isbn)" :book="book"></BookPreview>
+    </div>
+  </div>
 </template>
 
 <script>
+import {db} from "../db";
+import BookPreview from "./BookPreview";
+
 export default {
-  name: "Library.vue"
+  name: "Library.vue",
+  components: {BookPreview},
+  data() {
+    return {
+      booksList: [],
+      loading: false
+    }
+  },
+  setup() {
+    return {
+      db,
+    };
+  },
+  mounted() {
+    this.loading = true;
+    this.getAllBooksFromDatabase();
+  },
+  methods: {
+    getAllBooksFromDatabase: function() {
+      this.db.books.toArray().then(
+          (books) => {
+            this.booksList = books;
+            this.loading = false;
+          }
+      )
+    },
+    showBooksDetails(bookISBN) {
+      this.$router.push({
+        name: "Details",
+        params: { id: bookISBN },
+      });
+    },
+    async getAllTagsFromDatabase() {
+      console.log(await this.db.books.where("tags").equals("test").toArray());
+    }
+  }
 }
 </script>
 
