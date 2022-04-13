58ea84672d3cf077ae6cbc90fc20b07bac6b1766
perretpy <pierreyves.perret@gmail.com>
2022-04-13 18:33:37 +0200

    Remove style when book isn't in library

--- src/components/BookPreview.vue
+++ src/components/BookPreview.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="card">
-    <header class="card-header">
+    <header v-if="book.isbn" class="card-header">
       <p class="card-header-title">
         <i class="fa fa-book" aria-hidden="true" :style="book.read ? '' : 'color: rgba(218, 0, 0, 0.5)'"></i>
         <span v-if="book.read">Lu</span>
         <span v-else>Non lu</span>
       </p>
     </header>
-    <div class="card-content" :style="book.available ? 'background-color: rgba(0, 218, 0, 0.11)' : 'background-color: rgba(218, 0, 0, 0.11)'">
+    <div class="card-content" :style="book.isbn ? book.available ? 'background-color: rgba(0, 218, 0, 0.11)' : 'background-color: rgba(218, 0, 0, 0.11)' : 'background-color: rgba(185, 185, 185, 0.11)'">
       <div class="media">
         <div class="media-content">
           <p class="title is-4">{{ book.title }}</p>
